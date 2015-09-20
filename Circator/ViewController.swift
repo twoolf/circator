31427a86fd5f52a9c356abcd11f721ec60422ed6
Yanif Ahmad <yanif@jhu.edu>
2015-09-20 15:06:41 -0400

    Initial Commit

---
+++ Circator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Circator
+//
+//  Created by Yanif Ahmad on 9/20/15.
+//  Copyright © 2015 Yanif Ahmad, Tom Woolf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
