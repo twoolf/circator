773e48c0a9bba42eef1cc5f3df1100d9a322185d
twoolf <twoolf@jhu.edu>
2015-10-02 07:42:21 -0400

    added readout of Body Mass from HKSampleType

--- Circator/SettingsViewController.swift
+++ Circator/SettingsViewController.swift
@@ -18,8 +18,10 @@ class SettingsViewController: UIViewController {
   let healthManager = HealthManager()
   let kUnknownString = "Unknown"
   var height : HKQuantitySample?
+  var bodymass : HKQuantitySample?
 
   @IBOutlet var heightLabel:UILabel!
+  @IBOutlet var bodymassLabel:UILabel!
 
   override func viewDidLoad() {
       super.viewDidLoad()
@@ -31,9 +33,19 @@ class SettingsViewController: UIViewController {
       heightLabel.textAlignment = .Center
       heightLabel.font = UIFont.systemFontOfSize(20)
       heightLabel.frame = CGRectMake(0, 100, 300, 100)
+    
+      bodymassLabel = UILabel()
+      bodymassLabel.text = kUnknownString
+      bodymassLabel.textColor = UIColor.blackColor()
+      bodymassLabel.textAlignment = .Center
+      bodymassLabel.font = UIFont.systemFontOfSize(20)
+      bodymassLabel.frame = CGRectMake(0, 150, 300, 100)
 
       self.view.addSubview(heightLabel)
       updateHeight()
+    
+      self.view.addSubview(bodymassLabel)
+      updateBodyMass()
     
       let uploadButton = UIButton()
       uploadButton.setTitle("Upload", forState: .Normal)
@@ -61,7 +73,7 @@ class SettingsViewController: UIViewController {
       }
       let json = ["samples":jsonArray]
       print("Device samples: " + json["samples"]!.count.description)
-      Alamofire.request(.POST, "http://damsl.cs.jhu.edu/circator/post", parameters: json, encoding: .JSON)
+      Alamofire.request(.POST, "http://45.55.194.186/post", parameters: json, encoding: .JSON)
   }
 
   func authorizeHealthKit()
@@ -108,5 +120,37 @@ class SettingsViewController: UIViewController {
         self.heightLabel.text = "HK Height: " + heightLocalizedString
       });
     })
-  }    
+  }
+
+    func updateBodyMass()
+    {
+        // 1. Construct an HKSampleType for bmi
+        let sampleType = HKSampleType.quantityTypeForIdentifier(HKQuantityTypeIdentifierBodyMass)!
+        
+        // 2. Call the method to read the most recent Height sample
+        self.healthManager.readMostRecentSample(sampleType, completion: { (mostRecentBodyMass, error) -> Void in
+            
+            if( error != nil )
+            {
+                print("Error reading body mass from HealthKit Store: \(error.localizedDescription)")
+                return;
+            }
+            
+            var bodymassLocalizedString = self.kUnknownString;
+            self.bodymass = mostRecentBodyMass as? HKQuantitySample;
+            
+            // 3. Format the body mass to display it on the screen
+            if let kilograms = self.bodymass?.quantity.doubleValueForUnit(HKUnit.gramUnitWithMetricPrefix(.Kilo)) {
+                let bodymassFormatter = NSMassFormatter()
+                bodymassFormatter.forPersonMassUse = true;
+                bodymassLocalizedString = bodymassFormatter.stringFromKilograms(kilograms);
+            }
+            
+            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                self.bodymassLabel.text = "HK Body Mass: " + bodymassLocalizedString
+            });
+        })
+    }    
+
+    
 }
