de933c65c6fe60e2243249e1b9250377ef169a01
Sihao Lu <lsh32768@gmail.com>
2015-10-29 07:20:41 -0400

    Implement Apple Watch preview screen and data transfer and display. But data does not persist: need App container.

--- Circator/AppDelegate.swift
+++ Circator/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CircatorKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
---
+++ CircatorKit/CircatorKit.h
@@ -0,0 +1,19 @@
+//
+//  CircatorKit.h
+//  CircatorKit
+//
+//  Created by Sihao Lu on 10/29/15.
+//  Copyright © 2015 Yanif Ahmad, Tom Woolf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CircatorKit.
+FOUNDATION_EXPORT double CircatorKitVersionNumber;
+
+//! Project version string for CircatorKit.
+FOUNDATION_EXPORT const unsigned char CircatorKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CircatorKit/PublicHeader.h>
+
+
