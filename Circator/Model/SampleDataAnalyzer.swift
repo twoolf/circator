5a6676acab0c0c887aa28bae68b96c6ac82ce42b
Tom Woolf <twoolf@jhu.edu>
2015-11-05 17:03:46 -0500

    bubble chart is now by 1/5 increments instead of by 1/3

--- Circator/Model/SampleDataAnalyzer.swift
+++ Circator/Model/SampleDataAnalyzer.swift
@@ -97,43 +97,67 @@ class SampleDataAnalyzer: NSObject {
             var size = summaryDataSorted[0]
 //            BubbleChartDataEntry(
             dataEntries.append(BubbleChartDataEntry(xIndex: 0, value: summaryDataSorted[0], size: CGFloat(summaryDataSorted[0]) ))
-// 1st third
+// 1st fifth
             var sum = 0.0
             var ave = 0.0
             var count = 0.0
-            let oneThird = summaryDataSorted.count/3
-            for i in 0..<oneThird {
+            let oneFifth = summaryDataSorted.count/5
+            for i in 0..<oneFifth {
                 sum = sum + summaryDataSorted[i]
                 count = count + 1.0
                 }
             ave = sum/count
-            xVals.append("1st third")
+            xVals.append("1st")
             dataEntries.append(BubbleChartDataEntry(xIndex: 1, value: ave, size: CGFloat(ave) ))
-// 2nd third
+// 2nd fifth
             sum = 0.0
             ave = 0.0
             count = 0.0
-            let twoThirds = 2*(summaryDataSorted.count/3)
-            for i in oneThird..<twoThirds {
+            let twoFifths = 2*(summaryDataSorted.count/5)
+            for i in oneFifth..<twoFifths {
                 sum = sum + summaryDataSorted[i]
                 count = count + 1.0
             }
             ave = sum/count
-            xVals.append("2nd third")
+            xVals.append("2nd")
             dataEntries.append(BubbleChartDataEntry(xIndex: 2, value: ave, size: CGFloat(ave) ))
+// 3rd fifth
+            sum = 0.0
+            ave = 0.0
+            count = 0.0
+            let threeFifths = 3*(summaryDataSorted.count/5)
+            for i in twoFifths..<threeFifths {
+                sum = sum + summaryDataSorted[i]
+                count = count + 1.0
+            }
+            ave = sum/count
+            xVals.append("3rd")
+            dataEntries.append(BubbleChartDataEntry(xIndex: 3, value: ave, size: CGFloat(ave) ))
+// 4th fifth
+            sum = 0.0
+            ave = 0.0
+            count = 0.0
+            let fourFifths = 4*(summaryDataSorted.count/5)
+            for i in threeFifths..<fourFifths {
+                sum = sum + summaryDataSorted[i]
+                count = count + 1.0
+            }
+            ave = sum/count
+            xVals.append("4th")
+            dataEntries.append(BubbleChartDataEntry(xIndex: 4, value: ave, size: CGFloat(ave) ))
 // last third
             sum = 0.0
             ave = 0.0
             count = 0.0
-            for i in twoThirds..<summaryDataSorted.count {
+            for i in fourFifths..<summaryDataSorted.count {
                 sum = sum + summaryDataSorted[i]
                 count = count + 1.0
             }
             ave = sum/count
-            xVals.append("last third")
-            dataEntries.append(BubbleChartDataEntry(xIndex: 3, value: ave, size: CGFloat(ave) ))
+            xVals.append("last fifth")
+            dataEntries.append(BubbleChartDataEntry(xIndex: 5, value: ave, size: CGFloat(ave) ))
             xVals.append("max")
-            dataEntries.append(BubbleChartDataEntry(xIndex: 4, value: summaryDataSorted.last!, size: CGFloat(summaryDataSorted.last!) ))
+            dataEntries.append(BubbleChartDataEntry(xIndex: 6, value: summaryDataSorted.last!, size: CGFloat(summaryDataSorted.last!) ))
             let dataSet = BubbleChartDataSet(yVals: dataEntries)
             dataSetConfiguratorBubbleChart?(dataSet)
             return BubbleChartData(xVals: xVals, dataSet: dataSet)
