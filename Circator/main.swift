f2cf8549f93e42975fc3e8fd1896946ce65d38f0
Sihao Lu <lsh32768@gmail.com>
2015-12-07 18:48:44 -0500

    Add AppCore and Bridge to support consent

---
+++ Circator/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  Circator
+//
+//  Created by Sihao Lu on 12/7/15.
+//  Copyright © 2015 Yanif Ahmad, Tom Woolf. All rights reserved.
+//
+
+import UIKit
+
+autoreleasepool {
+    UIApplicationMain(Process.argc, Process.unsafeArgv, nil, NSStringFromClass(APCAppDelegate.self))
+}
