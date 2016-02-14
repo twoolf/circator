90a82725dd667241773cdd35d33eb42b1f467e72
Yanif Ahmad <yanif@jhu.edu>
2016-02-14 17:24:06 -0500

    Fix for pushing profiles during registration

--- Circator/View Controller/RegisterViewController.swift
+++ Circator/View Controller/RegisterViewController.swift
@@ -207,9 +207,18 @@ class RegisterViewController : FormViewController {
                 let subVC = ProfileSubviewController()
                 subVC.profileFields = fields
                 subVC.profilePlaceholders = placeholders
-                subVC.profileUpdater = { (k,v) in self.profileValues[k] = v }
+                subVC.profileUpdater = { kvv in self.updateProfile(kvv) }
                 subVC.subviewDesc = "\(text) profile"
                 self.navigationController?.pushViewController(subVC, animated: true)
+        }
+    }
+
+    func updateProfile(kvv: (String, String, UIViewController?)) {
+        if let mappedK = UserProfile.profileMapping[kvv.0] {
+            UserManager.sharedManager.pushProfile([mappedK:kvv.1], completion: {_ in return})
+        } else {
+            log.error("No mapping found for profile update: \(kvv.0) = \(kvv.1)")
+            if let vc = kvv.2 { UINotifications.genericError(vc, msg: "Invalid profile field") }
         }
     }
 }
