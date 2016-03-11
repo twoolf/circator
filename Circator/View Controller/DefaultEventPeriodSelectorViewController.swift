9936303e49037918b2737af811d76de6e0c5e5f0
elwhitman <ewhitman@college.harvard.edu>
2016-03-10 22:40:21 -0500

    working on defaults settings controls

--- Circator/View Controller/DefaultEventPeriodSelectorViewController.swift
+++ Circator/View Controller/DefaultEventPeriodSelectorViewController.swift
@@ -16,35 +16,95 @@ import HTPressableButton
 import Crashlytics
 import SwiftDate
 
+enum TimeOfDayInterval {
+    case start
+    case end
+}
+
+/*
 //minutes of hour when user awakens and goes to sleep
-let wakeMinute = 0
-let sleepMinute = 0
+var wakeMinute = 0
+var sleepMinute = 0
 
 //hour of day when user awakens and goes to sleep
-let wakeHour = 8
-let sleepHour = 22
+var wakeHour = 8
+var sleepHour = 22
 
 
 //specific date for time when user awakens and goes to sleep
-let wakeTime = (wakeHour.hours + wakeMinute.minutes).fromDate(today())
-let sleepTime = (sleepHour.hours + sleepMinute.minutes).fromDate(today())
+let wakeTime = (wakeHour.hours + wakeMinute.minutes).fromDate(NSCalendar.currentCalendar().startOfDayForDate(NSDate()))
+let sleepTime = (sleepHour.hours + sleepMinute.minutes).fromDate(NSCalendar.currentCalendar().startOfDayForDate(NSDate()))
 
 //offset hours and minutes of lunch and dinner start times, note: breakfast is just awake to lunch
-let lunchOffsetMinute = 0
-let dinnerOffsetMinute = 0
+var lunchOffsetMinute = 0
+var dinnerOffsetMinute = 0
 
-let lunchOffsetHour = 11
-let dinnerOffsetHour = 18
+var lunchOffsetHour = 11
+var dinnerOffsetHour = 18
+*/
 
-class DefaultEventPeriodSelectorViewController : UIViewController {
+var AwakeInterval: TimeOfDayInterval?
+var AsleepInterval: TimeOfDayInterval?
+
+var BreakfastInterval: TimeOfDayInterval?
+var LunchInterval: TimeOfDayInterval?
+var DinnerInterval: TimeOfDayInterval?
+
+let sections : [String: [String: Int]] = ["Circadian Rhythmn": ["Awake": 1, "Asleep": 2], "Meal Periods": ["Breakfast": 3, "Lunch": 4, "Dinner": 5]]
+
+
+class DefaultEventPeriodSelectorViewController : UITableViewController {
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.setNavigationBarHidden(false, animated: false)
         navigationItem.title = "Defaults"
+        tableView.reloadData()
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "defaultIntervalSettingsCell")
+    }
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return sections.count
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let keys = [String](sections.keys)
+        return section < sections.count ? sections[keys[section]]!.count : 0
+    }
+    
+    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        let keys = [String](sections.keys)
+        return section < sections.count ? keys[section] : ""
+    }
+    
+    func getSubviewTitle(section : Int, row: Int) -> String {
+        let keys = [String](sections[([String](sections.keys))[section]]!.keys)
+        return keys[row]
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("defaultIntervalSettingsCell", forIndexPath: indexPath)
+        cell.preservesSuperviewLayoutMargins = false
+        //cell.separatorInset = UIEdgeInsetsZero
+        //cell.layoutMargins = UIEdgeInsetsZero
         
+        let subview = FormLabelCell()
+        subview.formTextLabel()?.text = getSubviewTitle(indexPath.section, row: indexPath.row)
+        cell.contentView.addSubview(subview)
+
+        let timeInterval : [Int] = [1, 2]
+        let timeIntervalText = FormLabelCell()
+        let timeIntervalTextLabel = timeIntervalText.formTextLabel()
+        timeIntervalTextLabel?.text = "\(timeInterval[0])" + " to " + "\(timeInterval[1])"
+        timeIntervalTextLabel?.textAlignment = .Right
+        cell.contentView.addSubview(timeIntervalText)
+        
+        cell.imageView?.image = nil
+        cell.accessoryType = .DisclosureIndicator
+        return cell
     }
 }
