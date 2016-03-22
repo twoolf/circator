879060f5006a0ac8e7993b3153f2d374c307ecd7
Mariano Pennini <mpennin3@jhu.edu>
2016-03-22 18:03:53 -0400

    Added functionality for Meal entry, just need to link the data to HealthKit.

--- CircatorWatch Extension/MealInterfaceController.swift
+++ CircatorWatch Extension/MealInterfaceController.swift
@@ -12,10 +12,15 @@ import Foundation
 
 class MealInterfaceController: WKInterfaceController {
     
-    
+    @IBOutlet var MinusMealTimeButton: WKInterfaceButton!
+    @IBOutlet var EnterMealTimeButton: WKInterfaceButton!
+    @IBOutlet var PlusMealTimeButton: WKInterfaceButton!
+    @IBOutlet var timeLabel: WKInterfaceLabel!
+    var time = 0.00
+
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
-        
+        updateConfiguration()
         // Configure interface objects here.
     }
 
@@ -23,7 +28,32 @@ class MealInterfaceController: WKInterfaceController {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
     }
+    
+   
+    @IBAction func onPlusButton() {
+        time++
+        updateConfiguration()
+        
+    }
+    
+    @IBAction func onMinusButton() {
+        if(time == 0) {
+            updateConfiguration()
+        } else{
+            time--
+            updateConfiguration()
+        }
+    }
+    
+    func updateConfiguration() {
+        timeLabel.setText("\(time)")
+    }
 
+    @IBAction func onEnterButton() {
+        
+    }
+    
+    
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
