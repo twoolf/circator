1c9c96bc55c4ca0126535293ad30380c020f0cb3
Mariano Pennini <mpennin3@jhu.edu>
2016-03-30 15:31:53 -0400

    Replaced MiscInterfaceController with WaterInterfaceController and added functionality for adding cups of water per day

--- CircatorWatch Extension/WaterInterfaceController.swift
+++ CircatorWatch Extension/WaterInterfaceController.swift
@@ -12,20 +12,53 @@ import Foundation
 
 class WaterInterfaceController: WKInterfaceController {
 
+    @IBOutlet var minusButton: WKInterfaceButton!
+    @IBOutlet var plusButton: WKInterfaceButton!
+    @IBOutlet var waterLabel: WKInterfaceLabel!
+    @IBOutlet var enterButton: WKInterfaceButton!
+    var cups = 0.00
+        
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
-        
+        updateConfiguration()
         // Configure interface objects here.
     }
-
+        
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
     }
-
+        
+        
+    @IBAction func onPlusButton() {
+        cups++
+        updateConfiguration()
+            
+    }
+        
+    @IBAction func onMinusButton() {
+        if(cups == 0) {
+            updateConfiguration()
+        } else{
+            cups--
+            updateConfiguration()
+        }
+    }
+        
+    func updateConfiguration() {
+        waterLabel.setText("\(cups)")
+    }
+        
+    @IBAction func onEnterButton() {
+        //send time to healthKit and reset time to 0.00
+    }
+        
+        
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
     }
+        
+
 
 }
