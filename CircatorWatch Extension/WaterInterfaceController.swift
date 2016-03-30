2b5f7130facf308a132a0c8aa79d9cc693b320e5
Mariano Pennini <mpennin3@jhu.edu>
2016-03-30 15:31:14 -0400

    Replaced MiscInterfaceController with WaterInterfaceController

---
+++ CircatorWatch Extension/WaterInterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  WaterInterfaceController.swift
+//  Circator
+//
+//  Created by Mariano on 3/30/16.
+//  Copyright © 2016 Yanif Ahmad, Tom Woolf. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class WaterInterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
