42f4f518255a37dce23d5c5a5cee2b744b76afaa
Artem Usachov <artyom.usachev@srost-studio.com>
2016-04-22 14:11:41 +0300

    RegisterLoginLandingViewController added

---
+++ MetabolicCompass/View Controller/RegisterLoginLandingViewController.swift
@@ -0,0 +1,25 @@
+//
+//  RegisterLoginLandingViewController.swift
+//  MetabolicCompass
+//
+//  Created by Artem Usachov on 4/22/16.
+//  Copyright © 2016 Yanif Ahmad, Tom Woolf. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class RegisterLoginLandingViewController: UIViewController {
+    
+    @IBOutlet weak var logoTopMargin: NSLayoutConstraint!
+    @IBOutlet weak var registerButtonBottomMargin: NSLayoutConstraint!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if(UIScreen.mainScreen().nativeBounds.height == 960) {
+            self.logoTopMargin.constant = 5;
+            self.registerButtonBottomMargin.constant = 30;
+        }
+    }
+}
