6a6582370d18a348f8c180f461fdb96d5bd64727
Artem Usachov <artyom.usachev@srost-studio.com>
2016-04-25 12:45:28 +0300

    Images for login screen added LoginModel added (to handle table delegate and user credentials textfields) Table cell with textfield and image added

---
+++ MetabolicCompass/Model/LoginModel.swift
@@ -0,0 +1,9 @@
+//
+//  LoginModel.swift
+//  MetabolicCompass
+//
+//  Created by Artem Usachov on 4/25/16.
+//  Copyright © 2016 Yanif Ahmad, Tom Woolf. All rights reserved.
+//
+
+import Foundation
---
+++ MetabolicCompass/View/InputTableCellWithImage.swift
@@ -0,0 +1,9 @@
+//
+//  InputTableCellWithImage.swift
+//  MetabolicCompass
+//
+//  Created by Artem Usachov on 4/22/16.
+//  Copyright © 2016 Yanif Ahmad, Tom Woolf. All rights reserved.
+//
+
+import Foundation
