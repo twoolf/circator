256a31b01207d64e01ef576e463d3ac5f916d5df
Andre <Grim.on@yandex.ru>
2016-05-13 17:08:27 +0300

    (WIP) Task #MC-32 - Manage Balance screen UI development

---
+++ MetabolicCompass/View Controller/TabController/BalanceSampleListController.swift
@@ -0,0 +1,119 @@
+//
+//  BalanceSampleListController.swift
+//  MetabolicCompass
+//
+//  Created by Inaiur on 5/13/16.
+//  Copyright © 2016 Yanif Ahmad, Tom Woolf. All rights reserved.
+//
+
+import UIKit
+import HealthKit
+import MetabolicCompassKit
+
+class BalanceSampleListController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var data: [DashboardMetricsConfigItem] = [] {
+        didSet {
+            self.tableView.reloadData()
+        }
+    }
+    
+    private let appearanceProvider = DashboardMetricsAppearanceProvider()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.tableView.dataSource = self;
+        self.tableView.delegate   = self;
+        
+        self.data = []
+        
+        for type in PreviewManager.supportedTypes {
+            let active = PreviewManager.previewSampleTypes.contains(type)
+            self.data.append(DashboardMetricsConfigItem(type: type.identifier, active: active, object: type))
+        }
+        
+        
+    }
+    
+    func save() {
+        
+        var samples = [HKSampleType]()
+        
+        for item in self.data {
+            if (item.active) {
+                samples.append(item.object)
+            }
+        }
+        
+        PreviewManager.updatePreviewSampleTypes(samples)
+    }
+    
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return .LightContent;
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onClose(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+        save()
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.data.count
+    }
+    
+    private let cellIdentifier = "DashboardManageCell"
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as! ManageDashboardCell
+        
+        let item = self.data[indexPath.row]
+        cell.showsReorderControl         = false
+        
+        cell.leftImageView.image = appearanceProvider.imageForSampleType(item.type, active: false)
+        cell.captionLabel.text   = appearanceProvider.titleForSampleType(item.type, active: false).string
+        
+        return cell;
+    }
+    
+    func  selectedItemsCount() -> Int {
+        var selected = 0
+        
+        for item in self.data {
+            if (item.active) {
+                selected += 1
+            }
+        }
+        
+        return selected
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+        let obj = tableView.cellForRowAtIndexPath(indexPath) as? ManageDashboardCell
+        guard let cell = obj else {
+            return
+        }
+        
+        let item = self.data[indexPath.row]
+        
+        if (item.active && self.selectedItemsCount() == 1) {
+            return
+        }
+        
+        item.active = !item.active
+    }
+
+}
