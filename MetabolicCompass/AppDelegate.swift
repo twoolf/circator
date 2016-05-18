99ebdeabe626c28c638e461b9d81e6d2078e2d3a
Vladimir <vlad.dudkin@srost-studio.com>
2016-05-18 19:02:12 +0300

    after merge fix

--- MetabolicCompass/AppDelegate.swift
+++ MetabolicCompass/AppDelegate.swift
@@ -33,7 +33,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 //        let themeColor = UIColor(red: 0.01, green: 0.41, blue: 0.22, alpha: 1.0)
 //
 
-        UIFont.printAllAppFonts()
         
         window = UIWindow(frame: UIScreen.mainScreen().bounds)
         window?.backgroundColor = UIColor.whiteColor()
