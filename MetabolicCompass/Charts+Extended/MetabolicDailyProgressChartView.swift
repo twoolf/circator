1affd6693e2bf3d23701c4adf69c51ed1b6424a0
ArtemUsachev <artyom.usachev@srost-studio.com>
2016-05-20 12:49:36 +0300

    MC-35

--- MetabolicCompass/Charts+Extended/MetabolicDailyProgressChartView.swift
+++ MetabolicCompass/Charts+Extended/MetabolicDailyProgressChartView.swift
@@ -8,39 +8,35 @@
 
 import Foundation
 import Charts
+import MetabolicCompassKit
 
-class MetabolicDailyPorgressChartView : HorizontalBarChartView {
+class MetabolicDailyPorgressChartView : HorizontalBarChartView, DailyChartModelProtocol {
     
     class var exerciseColor: UIColor {
-        return UIColor.colorWithHexString("#009A00", alpha: 0.7)!
+        return UIColor.colorWithHexString("#20990b", alpha: 0.7)!
     }
     
     class var eatingColor: UIColor {
-        return UIColor.colorWithHexString("#E4442D", alpha: 0.7)!
+        return UIColor.colorWithHexString("#e84c2c", alpha: 0.7)!
     }
     
     class var sleepColor: UIColor {
-        return UIColor.colorWithHexString("#008BFF", alpha: 0.7)!
+        return UIColor.colorWithHexString("#338aff", alpha: 0.7)!
     }
     
     class var fastingColor: UIColor {
-        return UIColor.colorWithHexString("#001844", alpha: 0.7)!
+        return UIColor.colorWithHexString("#021e45", alpha: 0.7)!
     }
     
-    private let dailyChartModel = DailyChartModel()
-    
     func prepareChart () {
-        
-//        dailyChartModel.getLastSevenDays()
-        
+        self.descriptionText = ""
         self.drawValueAboveBarEnabled = true
         self.drawBarShadowEnabled = false
         self.maxVisibleValueCount = 24
-        
         let xAxis = self.xAxis;
         xAxis.labelPosition = .Bottom;
         xAxis.labelTextColor = UIColor.colorWithHexString("#ffffff", alpha: 0.3)!
-        xAxis.labelFont = UIFont.systemFontOfSize(10)
+        xAxis.labelFont = ScreenManager.sharedInstance.appFontOfSize(12.0)
         xAxis.drawAxisLineEnabled = true
         xAxis.drawGridLinesEnabled = true
         xAxis.gridLineWidth = 0;
@@ -50,52 +46,13 @@ class MetabolicDailyPorgressChartView : HorizontalBarChartView {
         leftAxis.axisMinValue = 0.0
         leftAxis.axisMaxValue = 24.0
         
-        self.legend.formSize = 0;
-        self.legend.font = UIFont.systemFontOfSize(0)
-//        self.contentView.legend.xEntrySpace = 1.0;
-        self.updateChartData()
-    }
-    
-    private func updateChartData () {
-        let exerciseColor = UIColor.greenColor()
-        let eatColor = UIColor.redColor()
-        let sleepColor = UIColor.blueColor()
-        let fastingColor = UIColor.clearColor()
-        //days
-        let days = ["", "", "", "", "", "", ""]
-//        var colors:[UIColor] = []
-        var entriesArray: [BarChartDataEntry] = []
-        let valuesArr = [[2.0, 3.0, 4.0, 3.0, 2.0, 0.2, 0.7],
-                         [3.0, 5.0, 3.6, 2.0, 1.0, 0.2, 0.7],
-                         [1.0, 2.0, 6.0, 4.0, 1.5, 0.2, 0.7],
-                         [4.0, 3.2, 4.4, 3.0, 2.0, 0.2, 0.7],
-                         [0.0, 3.0, 4.0, 3.1, 2.2, 0.2, 0.7],
-                         [1.2, 2.1, 4.3, 3.4, 2.0, 0.2, 0.7],
-                         [2.9, 3.3, 4.6, 3.7, 2.8, 0.9, 0.7]]
-        
-        for (index, values) in valuesArr.enumerate() {
-            let entry = BarChartDataEntry.init(values: values, xIndex: index)
-            entriesArray.append(entry)
-        }
-        
-        let set = BarChartDataSet.init(yVals: entriesArray, label: nil)
-        set.barSpace = 0.3
-        set.drawValuesEnabled = false
-        set.colors = [fastingColor, exerciseColor, sleepColor, eatColor]
-        
-        let data = BarChartData.init(xVals: days, dataSets: [set])
-        self.data = data
-        
         let formatter = NSNumberFormatter()
         formatter.positiveFormat = "#"
         formatter.locale = NSLocale.currentLocale()
         
         let rightAxis = self.rightAxis
-        rightAxis.axisMinValue = max(0.0, self.data!.yMin - 1.0)
-        rightAxis.axisMaxValue = min(23.0, self.data!.yMax + 1.0)
-        rightAxis.labelCount = Int(rightAxis.axisMaxValue - rightAxis.axisMinValue)
         rightAxis.labelTextColor = UIColor.colorWithHexString("#ffffff", alpha: 0.3)!
-        rightAxis.labelFont = UIFont.systemFontOfSize(12)
+        rightAxis.labelFont = ScreenManager.sharedInstance.appFontOfSize(12.0)
         rightAxis.drawAxisLineEnabled = true
         rightAxis.drawGridLinesEnabled = true
         rightAxis.valueFormatter = formatter
@@ -104,5 +61,31 @@ class MetabolicDailyPorgressChartView : HorizontalBarChartView {
         rightAxis.gridLineWidth = 1
         rightAxis.gridLineDashPhase = 1
         rightAxis.gridLineDashLengths = [3.0]
+        
+        self.legend.formSize = 0;
+        self.legend.font = UIFont.systemFontOfSize(0)
+    }
+    
+    func updateChartData (valuesArr: [[Double]], chartColorsArray: [[UIColor]]) {
+        //days
+        let days = ["", "", "", "", "", "", ""]
+        var dataSetArray: [BarChartDataSet] = []
+        for (index, values) in valuesArr.enumerate() {
+            let entry = BarChartDataEntry.init(values: values, xIndex: index)
+            let set = BarChartDataSet.init(yVals: [entry], label: nil)
+            set.barSpace = 55
+            set.drawValuesEnabled = false
+            set.colors = chartColorsArray[index]
+            dataSetArray.append(set)
+        }
+        let data = BarChartData.init(xVals: days, dataSets: dataSetArray)
+        data.groupSpace = 75
+        self.data = data
+        
+        let rightAxis = self.rightAxis
+        rightAxis.axisMinValue = max(0.0, self.data!.yMin - 1.0)
+        rightAxis.axisMaxValue = min(24.0, self.data!.yMax + 1.0)
+        rightAxis.labelCount = Int(rightAxis.axisMaxValue - rightAxis.axisMinValue)
+        self.animate(yAxisDuration: 1.0)
     }
 }
