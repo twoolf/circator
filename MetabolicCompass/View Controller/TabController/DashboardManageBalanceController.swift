445010e801471445b58b003285caba66be279577
ArtemUsachev <artyom.usachev@srost-studio.com>
2016-05-25 10:59:39 +0300

    MC-247

--- MetabolicCompass/View Controller/TabController/DashboardManageBalanceController.swift
+++ MetabolicCompass/View Controller/TabController/DashboardManageBalanceController.swift
@@ -37,7 +37,8 @@ class DashboardManageBalanceController: UIViewController, UITableViewDelegate, U
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName : UIColor.whiteColor()]
+        self.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName : ScreenManager.sharedInstance.appNavBarTextColor(),
+                                                  NSFontAttributeName : ScreenManager.appNavBarFont()]
         self.navigationBar.tintColor = UIColor.whiteColor()
         self.navigationBar.barStyle = UIBarStyle.Black;
         
