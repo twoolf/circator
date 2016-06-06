9ff0cdeeaab5a61049bf054efe98240dc0409687
ArtemUsachev <artyom.usachev@srost-studio.com>
2016-06-06 13:15:05 +0300

    Add meal data source added

---
+++ MetabolicCompass/DataSources/AddMealDataSource.swift
@@ -0,0 +1,17 @@
+//
+//  AddMealDataSource.swift
+//  MetabolicCompass
+//
+//  Created by Artem Usachov on 6/6/16.
+//  Copyright © 2016 Yanif Ahmad, Tom Woolf. All rights reserved.
+//
+
+import UIKit
+
+class AddMealDataSource: NSObject, UITableViewDataSource {
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+}
