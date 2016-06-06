abfd23fed045681294a3aadad4cf2e6cc829472a
ArtemUsachev <artyom.usachev@srost-studio.com>
2016-06-06 13:14:33 +0300

    When cell added

---
+++ MetabolicCompass/View/Cells/WhenTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  WhenTableViewCell.swift
+//  MetabolicCompass
+//
+//  Created by Artem Usachov on 6/6/16.
+//  Copyright © 2016 Yanif Ahmad, Tom Woolf. All rights reserved.
+//
+
+import UIKit
+
+class WhenTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
