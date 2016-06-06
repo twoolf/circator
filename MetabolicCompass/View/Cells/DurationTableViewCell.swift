17a0666743e06795dda94e9567f05bc8830a89ca
ArtemUsachev <artyom.usachev@srost-studio.com>
2016-06-06 12:15:03 +0300

    Base graphics and classes added for add events

---
+++ MetabolicCompass/View/Cells/DurationTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  DurationTableViewCell.swift
+//  MetabolicCompass
+//
+//  Created by Artem Usachov on 6/6/16.
+//  Copyright © 2016 Yanif Ahmad, Tom Woolf. All rights reserved.
+//
+
+import UIKit
+
+class DurationTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var dropdownImage: UIImageView!
+    @IBOutlet weak var durationLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ MetabolicCompass/View/Cells/PickerTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  PickerTableViewCell.swift
+//  MetabolicCompass
+//
+//  Created by Artem Usachov on 6/6/16.
+//  Copyright © 2016 Yanif Ahmad, Tom Woolf. All rights reserved.
+//
+
+import UIKit
+
+class PickerTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var pickerView: UIPickerView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
